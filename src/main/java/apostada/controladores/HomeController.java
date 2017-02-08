d4171f49e1dd580d25b08db6e6b5b796d0c124ad
Pablo <Pablo@Pablo-PC>
2017-02-08 11:06:02 +0100

    Página principal / controller cambiado unas cosas ya listo para probar

--- src/main/java/apostada/controladores/HomeController.java
+++ src/main/java/apostada/controladores/HomeController.java
@@ -1,22 +1,22 @@
 package apostada.controladores;
 
-import apostada.servicios.UsersService;
+//import apostada.servicios.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class GreetingController {
+public class HomeController {
 
-	@Autowired
-	private UsersService usersService;
+	//@Autowired
+	//private UsersService usersService;
 
 	@RequestMapping("/Home")
 	public String greeting(Model model) {
-		model.addAttribute("name", usersService.getNumUsers() + " users");
+		//model.addAttribute("name", usersService.getNumUsers() + " users");
 
-		return "index";
+		return "home";
 	}
 	
 }
