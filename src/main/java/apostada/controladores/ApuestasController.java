e6a3c292c4b88fbae9e114628f74817de59f1396
Alex <alx_95@yahoo.es>
2017-02-09 11:09:19 +0100

    fix

---
+++ src/main/java/apostada/controladores/ApuestasController.java
@@ -0,0 +1,18 @@
+package apostada.controladores;
+
+import apostada.servicios.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class ApuestasController {
+
+	@Autowired
+	private UsuarioService userService;
+
+	@RequestMapping("/apuestas")
+	public String index(Model model) {
+		return "apuestas";
+	}
+	
+}
