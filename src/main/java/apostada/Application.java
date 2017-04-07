4622799d9bbe87468ec323c3f57423bba9515a7e
Alex <alx_95@yahoo.es>
2017-04-07 13:49:55 +0200

    azure

--- src/main/java/apostada/Application.java
+++ src/main/java/apostada/Application.java
@@ -3,6 +3,7 @@ package apostada;
 import com.hazelcast.config.Config;
 import com.hazelcast.config.JoinConfig;
 import java.util.Collections;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +13,9 @@ import org.springframework.session.hazelcast.config.annotation.web.http.EnableHa
 @EnableHazelcastHttpSession
 public class Application {
 
+	@Value("${hazelcast.member}")
+	private String HAZELCAST_MEMBER;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
@@ -21,7 +25,7 @@ public class Application {
 		Config config = new Config();
 		JoinConfig joinConfig = config.getNetworkConfig().getJoin();
 		joinConfig.getMulticastConfig().setEnabled(false);
-		joinConfig.getTcpIpConfig().setEnabled(true).setMembers(Collections.singletonList("127.0.0.1"));
+		joinConfig.getTcpIpConfig().setEnabled(true).setMembers(Collections.singletonList(HAZELCAST_MEMBER));
 		return config;
 	}
 
